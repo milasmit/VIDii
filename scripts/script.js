458f46729c49ecb307820705bf65b2887348c824
milasmit <94301124+milasmit@users.noreply.github.com>
2022-11-07 11:51:29 +0100

    eerste werking van teksten

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,12 @@
 // JavaScript Document
 console.log("Howdy!");
+
+var eersteHoed = document.querySelector("ol:nth-of-type(2) li:nth-of-type(2) button")
+
+eersteHoed.addEventListener("click", tekstwolk)
+
+function tekstwolk(){
+    var tekstEersteHoed = document.querySelector("section:nth-of-type(2) article")
+
+    tekstEersteHoed.classList.toggle('visible')
+}
