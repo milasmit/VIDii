7684263cf054eee99d0830a6315ff02b48501b5a
milasmit <94301124+milasmit@users.noreply.github.com>
2022-11-09 09:13:06 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,96 +1,105 @@
-// JavaScript Document
-console.log("Howdy!");
+var allHoeden = document.querySelectorAll(".hoed");
 
-// variabelen
-var eersteHoed = document.querySelector("ol:nth-of-type(2) li:nth-of-type(2) button");
 
-var grijzeHoed = document.querySelector("section:first-of-type button:nth-of-type(2)");
-
-var oranjeHoed = document.querySelector("ol:nth-of-type(2) li:nth-of-type(7) button");
-
-var donkerblauweHoed = document.querySelector("section:first-of-type button:nth-of-type(1)");
-
-var bruineHoed = document.querySelector("ol:nth-of-type(2) li:nth-of-type(11) button");
-
-var rondeHoed = document.querySelector("ol:nth-of-type(2) li:nth-of-type(6) button");
-
-var deDoos = document.querySelector("ol:nth-of-type(2) li:nth-of-type(3) button");
-
-var groeneHoed = document.querySelector("ol:nth-of-type(2) li:nth-of-type(12) button");
+allHoeden.forEach( eenHoed => {
+    eenHoed.addEventListener("click", hoedOp);
+});
 
 
 
-// eventListeners
-eersteHoed.addEventListener("click", tekstwolk1);
-
-grijzeHoed.addEventListener("click", tekstwolk2);
-
-oranjeHoed.addEventListener("click", tekstwolk3);
-
-donkerblauweHoed.addEventListener("click", tekstwolk4);
-
-bruineHoed.addEventListener("click", tekstwolk5);
-
-rondeHoed.addEventListener("click", tekstwolk6);
-
-deDoos.addEventListener("click", tekstwolk7);
-
-groeneHoed.addEventListener("click", tekstwolk8);
+function hoedOp() {
+    // checken of goofy al een hoedje op heeft
+    // als die er is - die eerst afzetten
+    let actievHat = document.querySelector(".op");
+    if (actievHat) {
+        actievHat.click()
+    }
 
 
 
-// functions
-function tekstwolk1(){
-    var tekstEersteHoed = document.querySelector("section:nth-of-type(2) article:first-of-type");
+    // aan de gang met de hoed waarop geklikt is
+    let clickedHat = this;
 
-    tekstEersteHoed.classList.toggle('visible');
-    eersteHoed.classList.toggle('verplaatsen');
+    // kopie van de hoed maken
+    let newHat = clickedHat.cloneNode(true);
+    newHat.classList.add("op");
+    newHat.dataset.id = clickedHat.id;
+    newHat.id = "";
+    // luisteren als de gekopieerde hoed heengevlogen is
+    newHat.addEventListener("animationend", hoedIsEr, {once:true});
+    // luisteren als op gekopieerde hoed geklikt wordt
+    newHat.addEventListener("click", hoedAf);
+    
+    // de kopie aan de website toevoegen
+    document.body.appendChild(newHat);
+    // het kopie is nog ontzichtbaar
+
+    // het origineel verstoppen
+    clickedHat.classList.add("hide");
+
+    // bepalen waar originele hoedje staat
+    let firstPos = clickedHat.getBoundingClientRect();
+    // bepalen waar gekopieerde hoedje staat
+    let lastPos = newHat.getBoundingClientRect();
+
+    // uitrekenen hoe ver het origineel en kopie van elkaar staan
+    let deltaX = firstPos.left - lastPos.left;
+    let deltaY = firstPos.top - lastPos.top;
+    // die afstand toevoegen aan de kopie
+    newHat.style = "--deltaX:" + deltaX + "; --deltaY:" + deltaY + ";";
+    // de kopie die afstand laten afleggen - met css
+    newHat.classList.add("vlieg");
 }
 
-function tekstwolk2(){
-    var tekstTweedeHoed = document.querySelector("section:nth-of-type(2) article:nth-of-type(2)")
 
-    tekstTweedeHoed.classList.toggle('visible')
-    grijzeHoed.classList.toggle('verplaatsen')
+
+function hoedIsEr() {
+    var cloneHat = this; 
+    // tekstwolk opzoeken
+    let deTekstwolk = document.querySelector("article." +  cloneHat.dataset.id);
+    // tekstwolk tonen
+    deTekstwolk.classList.add('visible');
 }
 
-function tekstwolk3(){
-    var tekstDerdeHoed = document.querySelector("section:nth-of-type(2) article:nth-of-type(3)")
 
-    tekstDerdeHoed.classList.toggle('visible')
-    oranjeHoed.classList.toggle('verplaatsen')
+
+function hoedAf() {
+    var cloneHat = this; 
+    var deTekstwolk = document.querySelector("article." + cloneHat.dataset.id);
+
+    // tekstwolk verbergen
+    deTekstwolk.classList.remove('visible');
+
+    // de orginele hoed opzoeken
+    let oriHat =  document.querySelector("#" + cloneHat.dataset.id);
+
+    // luisteren als de gekopieerde hoed teruggevlogen is
+    cloneHat.addEventListener("animationend", hoedIsTerug);
+
+    // bepalen waar originele hoedje staat
+    let oriPos = oriHat.getBoundingClientRect();
+    // bepalen waar gekopieerde hoedje staat
+    let clonePos = cloneHat.getBoundingClientRect();
+
+    // uitrekenen hoe ver het origineel en kopie van elkaar staan
+    let deltaX = oriPos.left - clonePos.left;
+    let deltaY = oriPos.top - clonePos.top;
+    // die afstand toevoegen aan de kopie
+    cloneHat.style = "--deltaX:" + deltaX + "; --deltaY:" + deltaY + ";";
+    // de kopie die afstand laten afleggen - met css
+    cloneHat.classList.add("terug");
 }
 
-function tekstwolk4(){
-    var tekstVierdeHoed = document.querySelector("section:nth-of-type(2) article:nth-of-type(4)")
 
-    tekstVierdeHoed.classList.toggle('visible')
-    donkerblauweHoed.classList.toggle('verplaatsen')
-}
 
-function tekstwolk5(){
-    var tekstVijfdeHoed = document.querySelector("section:nth-of-type(2) article:nth-of-type(5)")
+function hoedIsTerug() {
+    var cloneHat = this;
 
-    tekstVijfdeHoed.classList.toggle('visible')
-    bruineHoed.classList.toggle('verplaatsen')
-}
+    // de orginele hoed opzoeken
+    let oriHat =  document.querySelector("#" + cloneHat.dataset.id);
+    // origele hoed weer tonen
+    oriHat.classList.remove("hide");
 
-function tekstwolk6(){
-    var tekstZesdeHoed = document.querySelector("section:nth-of-type(2) article:nth-of-type(6)")
-
-    tekstZesdeHoed.classList.toggle('visible')
-    rondeHoed.classList.toggle('verplaatsen')
-}
-
-function tekstwolk7(){
-    var tekstZevendeHoed = document.querySelector("section:nth-of-type(2) article:nth-of-type(7)")
-
-    tekstZevendeHoed.classList.toggle('visible')
-}
-
-function tekstwolk8(){
-    var tekstAchtsteHoed = document.querySelector("section:nth-of-type(2) article:nth-of-type(8)")
-
-    tekstAchtsteHoed.classList.toggle('visible')
-    groeneHoed.classList.toggle('verplaatsen')
+    // de clone verwijderen
+    cloneHat.remove();
 }
